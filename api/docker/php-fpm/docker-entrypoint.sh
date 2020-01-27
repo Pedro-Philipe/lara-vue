a5566f62736310ddfd243810e8c330d061a1dfcb
Pedro Philipe Alves de Oliveira <pedro.oliveira@basis.com.br>
2020-01-27 10:58:34 -0300

    [ADD] Adicionando permissões a pasta de cache

--- api/docker/php-fpm/docker-entrypoint.sh
+++ api/docker/php-fpm/docker-entrypoint.sh
@@ -5,6 +5,7 @@ echo "[ ****************** ] Starting Endpoint of Application [ ****************
 
 echo "Granting 777 access to laravel storage path"
 chmod 777 -R storage/
+chmod 777 -R bootstrap/cache/
 
 exec "$@"
 
